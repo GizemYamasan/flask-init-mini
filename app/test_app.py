2c3f311ba6c56b3f59ced7fee392208d763d050a
Gizem Yamasan <GizemYamasan@users.noreply.github.com>
2024-03-24 16:27:01 +0300

    Update test_app.py

--- app/test_app.py
+++ app/test_app.py
@@ -1,27 +1,27 @@
 import pytest
 
-from run import app
+from run import app as application
+
+
+@pytest.fixture()
+def app():
+    application.config.update({
+        "TESTING": True,
+    })
+    yield application
 
 
 @pytest.fixture
-def client():
-    app.config["TESTING"] = True
-    with app.test_client() as client:
-        yield client
+def client(app):   
+    return app.test_client()
+
+
+@pytest.fixture()
+def runner(app):
+    return app.test_cli_runner()
 
 
 def test_api(client):
-    response = client.get("/api/v1/movies/recommend")
+    response = client.get("/")
     assert response.status_code == 200
-    assert response.is_json
-    assert response.get_json()[0]["title"] != ""
-
-    response = client.get("/api/v1/movies/recommend?title=Kingpin")
-    assert response.status_code == 200
-    assert response.is_json
-    assert len(response.get_json()) >= 2
-
-    response = client.get("/api/v1/movies/recommend?title=Lost%20in%20Translation")
-    assert response.status_code == 200
-    assert response.is_json
-    assert len(response.get_json()) >= 5
+    assert b"Flask" in response.data
